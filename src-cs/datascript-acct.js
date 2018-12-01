21b89b21f1bb4d945b898debf9b5b3011fc4f046
Alfredo Herrera <alfredherr@gmail.com>
2018-11-30 17:17:27 -0700

    Forgot some files

---
+++ src-cs/datascript-acct.js
@@ -0,0 +1,6 @@
+
+var currencies = ["MXN", "USD", "EUD", "GBP", "THB"]
+var numberOfAccounts = 120
+for (var i = 0; i < numberOfAccounts ; ++i) {
+  console.log(`${i+10000}|${Math.round(Math.random()*1000)} ${currencies[Math.round(Math.random()*(currencies.length - 1))]}|Test Name`)
+}
