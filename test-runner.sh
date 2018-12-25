d08b3ef57aa2cb522c2136e932a8d06926c399af
Alfredo Herrera <alfredherr@gmail.com>
2018-12-24 17:23:17 -0700

    commented PS in runner until it's ready

--- test-runner.sh
+++ test-runner.sh
@@ -1,5 +1,6 @@
 clear
 
+echo ""
 echo "##### C# ##############"
 echo ""
 
@@ -17,6 +18,7 @@ cd src-rs
 rm output.txt
 cargo build --release &&\
 time cargo run --release
+cd ..
 
 echo ""
 echo "##### HASKELL #########"
@@ -28,31 +30,33 @@ stack build --fast &&\
 time stack run Main
 cd ..
 
-# echo ""
-# echo "##### PURESCRIPT ######"
-# echo ""
-
-# cd src-ps
-# rm output.txt
-# psc-package build &&\
-# pulp build &&\
-# time node -e "require('output/Main').main()"
-# cd ..
-
-
+echo ""
+echo "##### PURESCRIPT ######"
+echo ""
+cd src-ps
+rm output.txt
+psc-package build &&\
+pulp build &&\
+time node -e "require('output/Main').main()"
 cd ..
+
 echo ""
 echo "##### ELIXIR ##########"
 echo ""
 
-rm elixir-output.txt
-cd src-ex
-rm output.txt
-rm -rf test_elixir
-mix deps.get
-mix clean
-MIX_ENV=prod mix release
-mv _build/prod/rel/test_elixir .
-cd ..
-./src-ex/test_elixir/bin/test_elixir foreground
-
+### The following lines are in this order to make sure we can run repeatedly
+rm elixir-output.txt                                                    ####
+cd src-ex                                                               ####
+rm output.txt                                                           ####
+rm -rf test_elixir                                                      ####
+mix deps.get                                                            ####
+mix clean                                                               ####
+MIX_ENV=prod mix release                                                ####
+mv _build/prod/rel/test_elixir .                                        ####
+cd ..                                                                   ####
+## And yes, you have to hit ^C once it's done.                          ####
+##  But it will tell you that when it's done.                           ####
+./src-ex/test_elixir/bin/test_elixir foreground                         ####
+###  On Windows Uncomment line below instead                            ####
+#./src-ex/test_elixir/bin/test_elixir.bat foreground                    ####
+############################################################################
