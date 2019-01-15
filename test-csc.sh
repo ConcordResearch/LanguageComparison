f0808e712ef3997cf62986ad1fa3aaaca1f4b99c
Wenyang Yao <ywenyang@revoteksolution.com>
2019-01-15 14:53:52 -0700

    changed test-csc.sh

--- test-csc.sh
+++ test-csc.sh
@@ -9,3 +9,23 @@ rm output.txt
 dotnet build &&\
 time dotnet run
 cd ..
+
+echo ""
+echo "##### C# ##############"
+echo ""
+
+cd src-cs
+rm output.txt
+dotnet build &&\
+time dotnet run
+cd ..
+
+echo ""
+echo "##### RUST ############"
+echo ""
+
+cd src-rs
+rm output.txt
+cargo build --release &&\
+time cargo run --release
+cd ..
