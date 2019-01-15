6ef445769fdb02309af8cdf404dba08dd4de9d28
Wenyang Yao <ywenyang@revoteksolution.com>
2019-01-15 16:34:16 -0700

    changed

--- test-csc.sh
+++ test-csc.sh
@@ -29,3 +29,40 @@ rm output.txt
 cargo build --release &&\
 time cargo run --release
 cd ..
+
+echo ""
+echo "##### HASKELL #########"
+echo ""
+
+cd src-hs
+rm output.txt
+stack build --fast &&\
+time stack run Main
+cd ..
+
+
+echo ""
+echo "##### PURESCRIPT ######"
+echo ""
+cd src-ps
+rm output.txt
+psc-package build &&\
+pulp build -- --json-errors 2> errors.txt &&\
+time node --max-old-space-size=8192 -e "require('./output/Main').main()"
+cd ..
+
+echo ""
+echo "##### ELIXIR ##########"
+echo ""
+
+### The following lines are in this order to make sure we can run repeatedly
+cd src-ex                                                               ####
+rm -rf _build                                                           ####
+mix deps.get                                                            ####
+mix clean                                                               ####
+MIX_ENV=prod mix release                                                ####
+time ./_build/prod/rel/test_elixir/bin/test_elixir foreground           ####
+cd ..                                                                   ####
+###  On Windows Uncomment line below instead                            ####
+#./src-ex/test_elixir/bin/test_elixir.bat foreground                    ####
+############################################################################
